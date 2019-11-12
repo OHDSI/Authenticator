f900d52ec76a8cc627b87600324bfc895bc82cc3
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-11-12 14:52:30 +0300

    Added JWT token filter

--- src/main/java/org/ohdsi/authenticator/filter/JWTAuthenticationFilter.java
+++ src/main/java/org/ohdsi/authenticator/filter/JWTAuthenticationFilter.java
@@ -14,6 +14,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.ohdsi.authenticator.exception.AuthenticationException;
 import org.ohdsi.authenticator.service.authentication.Authenticator;
 import org.ohdsi.authenticator.util.AuthorizationUtils;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -24,8 +26,8 @@ import org.springframework.web.filter.GenericFilterBean;
 
 public class JWTAuthenticationFilter extends GenericFilterBean {
 
-    private static final String DEFAULT_ROLE = "USER";
-    private final Authenticator authenticator;
+    public static final String DEFAULT_ROLE = "USER";
+    protected final Authenticator authenticator;
 
     public JWTAuthenticationFilter(Authenticator authenticator) {
 
@@ -35,28 +37,50 @@ public class JWTAuthenticationFilter extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
         try {
-            HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
-            String authorize = httpRequest.getHeader(AuthorizationUtils.AUTHORIZE_HEADER);
-            if (StringUtils.isNotBlank(authorize) && authorize.startsWith(AuthorizationUtils.BEARER_AUTH)) {
-                String jwtToken = AuthorizationUtils.getToken(authorize);
+            String jwtToken = getToken(httpRequest);
+            if (StringUtils.isNotBlank(jwtToken)) {
                 String username = authenticator.resolveUsername(jwtToken);
                 if (Objects.nonNull(username) && SecurityContextHolder.getContext().getAuthentication() == null) {
-                    UserDetails userDetails = new User(username, "", getAuthorities(username));
+                    UserDetails userDetails = getUserDetails(username);
                     UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, jwtToken,
                             userDetails.getAuthorities());
+                    onSuccessAuthentication(httpRequest, userDetails, authentication);
                     SecurityContextHolder.getContext().setAuthentication(authentication);
                 }
             }
             filterChain.doFilter(servletRequest, servletResponse);
         } catch (AuthenticationException e) {
-            logger.error("Authentication error:", e);
+            String method = httpRequest.getMethod();
+            if (!HttpMethod.OPTIONS.matches(method)) {
+                if (logger.isDebugEnabled()) {
+                    logger.debug("Authentication failed", e);
+                } else {
+                    logger.error(String.format("Authentication failed: %s, requested: %s %s", e.getMessage(),
+                            method, httpRequest.getRequestURI()));
+                }
+            }
             HttpServletResponse response = (HttpServletResponse) servletResponse;
-            response.sendError(HttpServletResponse.SC_FORBIDDEN);
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
         }
     }
 
-    private Collection<? extends GrantedAuthority> getAuthorities(String username) {
+    protected String getToken(HttpServletRequest httpRequest) {
+
+        String authorize = httpRequest.getHeader(AuthorizationUtils.AUTHORIZE_HEADER);
+        return AuthorizationUtils.getToken(authorize);
+    }
+
+    protected void onSuccessAuthentication(HttpServletRequest request, UserDetails userDetails, AbstractAuthenticationToken authentication) {
+    }
+
+    protected UserDetails getUserDetails(String username) {
+
+        return new User(username, "", getAuthorities(username));
+    }
+
+    protected Collection<? extends GrantedAuthority> getAuthorities(String username) {
 
         return Collections.singletonList(new SimpleGrantedAuthority(DEFAULT_ROLE));
     }
